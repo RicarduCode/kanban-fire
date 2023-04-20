51b13e80722120710f4ed3cc9790e9c78cffacd1
RicarduCode <ricardosouzaseramim@gmail.com>
2023-04-20 11:17:20 -0300

    AULA-ANGULAR

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-cadd9',
+    appId: '1:223554080664:web:8830bbf571cee45ab2a5ed',
+    storageBucket: 'kanbanfire-cadd9.appspot.com',
+    apiKey: 'AIzaSyC5t8KV8Dy7ngH8gM2vevJTu6tfrhWtwvo',
+    authDomain: 'kanbanfire-cadd9.firebaseapp.com',
+    messagingSenderId: '223554080664',
+    measurementId: 'G-X9CN3HEXHK',
+  },};
